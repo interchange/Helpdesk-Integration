cd5a7baf8140c47433793e711b2451c61cdcdabb
Marco Pessotto <melmothx@gmail.com>
2017-09-07 11:22:24 +0200

    Be more flexible when building the EmailParser object

--- lib/Helpdesk/Integration/IMAP/EmailParser.pm
+++ lib/Helpdesk/Integration/IMAP/EmailParser.pm
@@ -3,6 +3,7 @@ package Helpdesk::Integration::IMAP::EmailParser;
 use strict;
 use warnings;
 use Types::Standard qw/Object InstanceOf Int/;
+use Email::MIME;
 use Moo;
 
 =head1 NAME
@@ -22,6 +23,37 @@ filenames, if needed.
 
 =cut
 
-has mail => (is => 'ro', isa => InstanceOf['Email::MIME'], required => 1);
+has mail => (is => 'ro',
+             isa => InstanceOf['Email::MIME'],
+             required => 1,
+             handles => [qw/header/]
+            );
 has id => (is => 'ro', isa => Int, default => sub { 0 });
+
+sub BUILDARGS {
+    my ($class, @args) = @_;
+    if (@args % 2 == 0) {
+        my %out = @args;
+        return \%out;
+    }
+    if (@args == 1) {
+        my $arg = shift @args;
+        my $mail;
+        if (ref($arg)) {
+            $mail = Email::MIME->new($$arg);
+        }
+        else {
+            local $/;
+            open (my $fh, '<', $arg) or die "Cannot open $arg";
+            my $body = <$fh>;
+            close $fh;
+            $mail = Email::MIME->new($body);
+        }
+        return { mail => $mail };
+    }
+    else {
+        die "Too many arguments. Accept either a named list or a single scalar with a filename or a reference to a scalar with the mail body";
+    }
+}
+
 1;
